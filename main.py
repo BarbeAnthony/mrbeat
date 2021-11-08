b5cac60447d6ed7e2e26eba4acb7f187bcbcff68
AnthonyBarbe <barbe.anthony33@gmail.com>
2021-11-08 14:37:11 +0100

    début du projet

---
+++ main.py
@@ -0,0 +1,14 @@
+from kivy.app import App
+from kivy.uix.relativelayout import RelativeLayout
+from kivy.uix.widget import Widget
+
+
+class MainWidget(RelativeLayout):
+    pass
+
+
+class MrBeatApp(App):
+    pass
+
+
+MrBeatApp().run()
