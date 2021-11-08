99de211040cf4ad467cc3b8883ff90e31f7ee143
AnthonyBarbe <barbe.anthony33@gmail.com>
2021-11-08 15:54:08 +0100

    Nombre de tracks et de steps via des variables

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.properties import ObjectProperty
 from kivy.uix.relativelayout import RelativeLayout
-from kivy.uix.widget import Widget
+from track import TrackWidget
 
 Builder.load_file("track.kv")
 
+NB_TRACKS = 4
+
 
 class MainWidget(RelativeLayout):
-    pass
+    tracks_layout = ObjectProperty(None)
+
+    def on_parent(self, widget, parent):
+        for i in range(0, NB_TRACKS):
+            self.tracks_layout.add_widget(TrackWidget())
 
 
 class MrBeatApp(App):
--- track.py
+++ track.py
@@ -1,5 +1,15 @@
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.togglebutton import ToggleButton
+
+NB_STEP_BUTTON = 16
 
 
 class TrackWidget(BoxLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        for i in range(0, NB_STEP_BUTTON):
+            self.add_widget(TrackStepButton())
+
+
+class TrackStepButton(ToggleButton):
     pass
