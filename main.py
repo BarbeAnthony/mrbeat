c7d781c8fcf0583f9f483dab59ea21d7e3a783e5
AnthonyBarbe <barbe.anthony33@gmail.com>
2021-11-08 15:16:39 +0100

    Création du track widget

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from kivy.app import App
+from kivy.lang import Builder
 from kivy.uix.relativelayout import RelativeLayout
 from kivy.uix.widget import Widget
+
+Builder.load_file("track.kv")
 
 
 class MainWidget(RelativeLayout):
