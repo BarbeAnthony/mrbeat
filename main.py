d16ae1e39aa6f8dd3e95bf132a1af1cb7969419e
AnthonyBarbe <barbe.anthony33@gmail.com>
2021-11-09 00:34:46 +0100

    ajout du fichier sound_kit_service.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.properties import ObjectProperty
 from kivy.uix.relativelayout import RelativeLayout
+
+from sound_kit_service import SoundKitService
 from track import TrackWidget
 
 Builder.load_file("track.kv")
@@ -11,6 +13,10 @@ NB_TRACKS = 4
 
 class MainWidget(RelativeLayout):
     tracks_layout = ObjectProperty(None)
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.sound_kit_service = SoundKitService()
 
     def on_parent(self, widget, parent):
         for i in range(0, NB_TRACKS):
