f4f16027bf9db46beffb63a336acbed2bfabd51e
AnthonyBarbe <barbe.anthony33@gmail.com>
2021-11-12 03:30:34 +0100

    relier les steps de la UI aux tracks

--- main.py
+++ main.py
@@ -17,10 +17,10 @@ class MainWidget(RelativeLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.sound_kit_service = SoundKitService()
-        kick_sound = self.sound_kit_service.get_sound_at(0)
+        # kick_sound = self.sound_kit_service.get_sound_at(0)
         self.audio_engine = AudioEngine()
         # self.audioengine.play_sound(kick_sound.samples)
-        self.audio_engine.create_track(kick_sound.samples, 60)
+        # self.audio_engine.create_track(kick_sound.samples, 60)
 
     def on_parent(self, widget, parent):
         for i in range(0, self.sound_kit_service.get_nb_tracks()):
--- track.py
+++ track.py
@@ -6,19 +6,34 @@ NB_STEP_BUTTON = 16
 
 
 class TrackWidget(BoxLayout):
-    def __init__(self, audio_engine, sound_in_track, **kwargs):
+    def __init__(self, audio_engine, sound, **kwargs):
         super().__init__(**kwargs)
         self.audio_engine = audio_engine
-        self.sound_in_track = sound_in_track
+        self.sound = sound
+        self.track_audio_source = self.audio_engine.create_track(self.sound.samples, 60)
         sound_button = TrackSoundButton()
-        sound_button.text = sound_in_track.displayname
+        sound_button.text = sound.displayname
         sound_button.on_press = self.on_sound_button_press
         self.add_widget(sound_button)
+        self.step_buttons = []
         for i in range(0, NB_STEP_BUTTON):
-            self.add_widget(TrackStepButton())
+            step_button = TrackStepButton()
+            step_button.bind(state=self.on_step_button_state)
+            self.step_buttons.append(step_button)
+            self.add_widget(step_button)
 
     def on_sound_button_press(self):
-        self.audio_engine.play_sound(self.sound_in_track.samples)
+        self.audio_engine.play_sound(self.sound.samples)
+
+    def on_step_button_state(self, widget, state):
+        steps = []
+        for i in range(0, NB_STEP_BUTTON):
+            if self.step_buttons[i].state == "down":
+                steps.append(1)
+            else:
+                steps.append(0)
+        self.track_audio_source.set_steps(steps)
+
 
 
 class TrackSoundButton(Button):
